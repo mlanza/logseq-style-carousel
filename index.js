8f7e2704f7da8428246c22df55f5a880b3da0584
Mario T. Lanza <mlanza@doesideas.com>
2022-03-12 13:10:52 -0500

    refine UX

--- index.js
+++ index.js
@@ -1,55 +1,69 @@
 const config = {
-  buttons: {
-    futures: {
-      refreshRate: 5,
-      hits: ["text-decoration: underline;", ""],
-      styles: [{
-        char: "\\eb3e",
-        query: `[:find (pull ?block [*])
-          :in $ ?start ?end
-          :where
-          (or
-            [?block :block/scheduled ?d]
-            [?block :block/deadline ?d])
-          [(> ?d ?start)]
-          [(< ?d ?end)]]`,
-        inputs: ["today 1", "today 30"],
-        selector: "div[blockid=\"$uuid\"]",
-        matches: /./,
-        style: `{display: none;}`
+  "buttons": {
+    "todos": {
+      "tooltip": "Completed tasks",
+      "refreshRate": 5,
+      "hits": ["text-decoration: underline;", ""],
+      "styles": [{
+        "char": "\\ecf0",
+        "style": "div[data-refs-self*='\"done\"']:not(:focus-within), div[data-refs-self*='\"canceled\"']:not(:focus-within) {display: none;}",
+        "hits": "div#main-content-container div[data-refs-self*='\"done\"'], div#main-content-container div[data-refs-self*='\"canceled\"']"
       },{
-        char: "\\eb3f",
-        query: `[:find (pull ?block [*])
-          :in $ ?start ?end
-          :where
-          (or
-            [?block :block/scheduled ?d]
-            [?block :block/deadline ?d])
-          [(> ?d ?start)]
-          [(< ?d ?end)]]`,
-        inputs: ["today 1", "today 30"],
-        selector: "div#main-content-container:hover div[blockid=\"$uuid\"]",
-        matches: /./,
-        style: `{text-decoration: underline wavy;}`
+        "char": "\\ea9a",
+        "style": "div#main-content-container:hover div[data-refs-self*='\"done\"'] span.inline, div#main-content-container:hover div[data-refs-self*='\"canceled\"'] span.inline {text-decoration: underline wavy;}",
+        "hits": "div#main-content-container div[data-refs-self*='\"done\"'], div#main-content-container div[data-refs-self*='\"canceled\"']"
       }]
     },
-    todos: {
-      refreshRate: 5,
-      hits: ["text-decoration: underline;", ""],
-      styles: [{
-        char: "\\ecf0",
-        style: `div[data-refs-self*='"done"']:not(:focus-within), div[data-refs-self*='"canceled"']:not(:focus-within) {display: none;}`,
-        hits: `div#main-content-container div[data-refs-self*='"done"'], div#main-content-container div[data-refs-self*='"canceled"']`
-      },{
-        char: "\\ea9a",
-        style: `div#main-content-container:hover div[data-refs-self*='"done"'] span.inline, div#main-content-container:hover div[data-refs-self*='"canceled"'] span.inline {text-decoration: underline wavy;}`,
-        hits: `div#main-content-container div[data-refs-self*='"done"'], div#main-content-container div[data-refs-self*='"canceled"']`
-      }]
-    }
   }
 };
 
 const state = {};
+
+//must install as local package...
+let parentAccess = true;
+
+try {
+  //...not possible if installed from marketplace.
+  top.document.querySelector("body");
+} catch {
+  parentAccess = false;
+}
+
+async function getPage(){
+  let tries = 30;
+  return new Promise(function(resolve, reject){
+    const iv = setInterval(async function(){
+      const page = await logseq.Editor.getCurrentPage();
+      if (page || tries <= 0) {
+        clearInterval(iv);
+        resolve(page);
+      }
+      tries--;
+      if (tries < 0) {
+        clearInterval(iv);
+        reject(page);
+      }
+    }, 500);
+  });
+}
+
+function setButton(key, char, tooltip){
+  logseq.App.registerUIItem('toolbar', {
+    key: `button-${key}`,
+    template: `
+    <a class="button carousel" title="${tooltip}" data-key="${key}" data-on-click="cycle">
+      <i class="ti ti-home"></i>
+    </a>
+    `,
+  });
+  logseq.provideStyle({
+    key: `icon-${key}`,
+    style: `
+    .carousel[data-key="${key}"] i:before {
+      content: "${char}";
+    }`
+  });
+}
 
 function today(os){
   const offset = os ? parseInt(os) : 0;
@@ -62,74 +76,80 @@ const fns = {
   today: today
 }
 
-async function cycle(el) {
-  const key = el.dataset.key,
-        st  = state[key],
-        cfg = config.buttons[key];
-  st.idx = cfg.styles[st.idx + 1] ? st.idx + 1 : 0;
-  await refresh(key, cfg, st);
-}
-
 function expandInput(input){
   const [named, ...args] = input.split(" ");
   const fn = fns[named];
   return fn ? fn.apply(null, args) : input;
 }
 
-async function findIds({query, inputs, selector, matches, style}){
+function match(patterns){
+  return function(block){
+    for(let pattern of patterns){
+      if (!pattern.test(block.content)){
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+async function cycle(el) {
+  const key = el.dataset.key,
+        st  = state[key],
+        btn = config.buttons[key];
+  st.idx = btn.styles[st.idx + 1] ? st.idx + 1 : 0;
+  await refresh(key, btn, st);
+}
+
+async function findIds({query, inputs, matches, hit, selector, rules}){
   const results = (await logseq.DB.datascriptQuery.apply(logseq.DB, [query].concat(inputs.map(expandInput))) || []).flat();
-  const hits = results.filter(function(block){
-    return !!matches && matches.test(block.content);
-  }).map(function(block){
+  const patterns = (matches || []).map(expandInput).map(function(pattern){
+    return new RegExp(pattern);
+  });
+  const uuids = results.filter(match(patterns)).map(function(block){
     return block.uuid.$uuid$;
-  }).map(function(uuid){
-    return selector.replace(/\$uuid/g, uuid);
+  });
+  const hits = uuids.map(function(uuid){
+    return hit.replace(/\@uuid/g, uuid);
   }).join(", ");
-  return {hits, style: hits + " " + style};
+  const style = uuids.map(function(uuid){
+    return (selector || hit).replace(/\@uuid/g, uuid);
+  }).join(", ") + " " + rules;
+  return {hits, style};
 }
 
 async function refresh(key, config, state){
-  const current = config.styles[state.idx];
-  const {query} = current;
-  const {char, style, hits, inputs, selector, matches} = Object.assign(current, query ? await findIds(current) : null);
-
+  const status = config.styles[state.idx];
+  const {query} = status;
+  const {char, tooltip, style, hits} = Object.assign(status, query ? await findIds(status) : null);
   state.hits = hits;
-  logseq.App.registerUIItem('toolbar', {
-    key: `button-${key}`,
-    template: `
-    <a class="button carousel" data-key="${key}" data-on-click="cycle">
-      <i></i>
-    </a>
-    `,
-  });
   logseq.provideStyle({
     key: `active-${key}`,
     style
   });
-  logseq.provideStyle({
-    key: `icon-${key}`,
-    style: `
-    .carousel[data-key="${key}"] i:before {
-      content: "${char}";
-    }`
-  });
+  setButton(key, char, tooltip || "");
+  detectHits(key, config, state);
 }
 
-function refreshHits(key, config, state){
-  (config.refreshRate || 0) > 0 && setInterval(function(){
-    if (state.hits) {
-      const el = top.document.querySelector(state.hits);
-      const style = el ? config.hits[0] : config.hits[1];
-      logseq.provideStyle({
-        key: `hits-${key}`,
-        style: `
-        .carousel[data-key="${key}"] i:before {
-          ${style};
-        }`
-      });
-    }
-  }, config.refreshRate * 1000);
+function detectHits(key, config, state){
+  if (state.hits) {
+    const el = top.document.querySelector(state.hits);
+    const style = el ? config.hits[0] : config.hits[1];
+    logseq.provideStyle({
+      key: `hits-${key}`,
+      style: `
+      .carousel[data-key="${key}"] i:before {
+        ${style};
+      }`
+    });
+  }
 }
+
+const refreshHits =  parentAccess ? function(key, config, state){
+  (config.refreshRate || 0) > 0 && setInterval(function(){
+    detectHits(key, config, state);
+  }, config.refreshRate * 1000);
+} : function(){};
 
 function createModel(){
   return {refresh, cycle};
@@ -141,9 +161,7 @@ async function main(){
   logseq.provideStyle({
     key: 'main',
     style: `.carousel {
-      display: block;
       font-size: 20px;
-      margin-top: 0;
     }
     .carousel i:before {
       font-family: tabler-icons;
@@ -153,11 +171,25 @@ async function main(){
   });
 
   for (let key in config.buttons) {
+    const {char, tooltip} = config.buttons[key].styles[0];
+    setButton(key, char, tooltip);
+  }
+
+  await getPage();
+
+  for (let key in config.buttons) {
     const btn = config.buttons[key],
           st  = state[key] = {idx: 0};
     await refresh(key, btn, st);
     refreshHits(key, btn, st);
   }
+
+  logseq.App.onRouteChanged(async function(e){
+    await getPage();
+    for (let key in config.buttons) {
+      refresh(key, config.buttons[key], state[key]);
+    }
+  });
 }
 
 logseq.ready(createModel(), main).catch(console.error.bind(console));
